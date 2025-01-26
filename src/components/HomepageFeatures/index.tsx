2c014c9722713361a334867406b2777a2398502a
John Mavrick <john@kortex.co>
2025-01-26 09:45:05 -0600

    add links for feature showcases, grow container

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -2,55 +2,61 @@ import type {ReactNode} from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import Link from '@docusaurus/Link';
 
 type FeatureItem = {
   title: string;
   Svg?: React.ComponentType<React.ComponentProps<'svg'>>;
   description: ReactNode;
+  link?: string;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'AI-Powered Note-Taking ✨',
     // Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Goldfish uses AI to help you purify your thoughts.
       </>
     ),
+    link: 'docs/category/tutorial---ai-features'
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Sync with Your Favorite Apps 🔄',
     // Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Goldfish integrates with apps like Obsidian to let your ideas flow freely.
       </>
     ),
+    link: 'docs/category/tutorial---integrations'
   },
-  {
-    title: 'Powered by React',
-    // Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+  // {
+  //   title: 'We need a third point lol',
+  //   // Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+  //   description: (
+  //     <>
+  //       I am not sure what it could be
+  //     </>
+  //   ),
+  // },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({title, Svg, description, link}: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col')}>
       {Svg && <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>}
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
         <p>{description}</p>
+        {link && <Link
+          className="button--md"
+          to={link}>
+          Learn more →
+        </Link>}
       </div>
     </div>
   );
